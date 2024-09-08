759deca3604ac818c59fe3b30452f98970a8258f
nandhuraju <nandhuraju6@gmail.com>
2024-09-08 23:50:40 +0530

    certificate Dapp

--- ui/src/layouts/MainLayout.jsx
+++ ui/src/layouts/MainLayout.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Navbar from '../components/Navbar'
-
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const MainLayout = () => {
   return (
-    <>
-      
-      <div className="bg-green-100  pt-4 min-h-screen flex-col">
+    <div className="flex flex-col min-h-screen">
+      <div className="bg-green-100 pt-4 flex-grow">
         <Navbar />
         <Outlet />
       </div>
-      <div className="text-center text-gray-500 bg-green-100">
+      <div className="text-center text-gray-500 bg-green-100 py-4">
         &copy; 2024 ChainCerts. All rights reserved.
       </div>
-    </>
+    </div>
   );
-}
+};
 
-export default MainLayout
+export default MainLayout;
